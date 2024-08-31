550ef3f1c6776db6e97fb24ca6715c179f0dde53
Devlab Solution Limited <49413046+RashidulHasanRatul@users.noreply.github.com>
2024-09-01 03:09:51 +0600

    Add Negative Test for login

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,10 +1,11 @@
-
+const { expect } = require("@playwright/test");
 class LoginPage {
     constructor(page) {
       this.page = page;
       this.emailInput = page.locator('input[type="email"]');
       this.passwordInput = page.locator('input[placeholder="Password"]');
       this.loginButton = page.locator('button[type="submit"]');
+      this.errorMessage = page.locator('.toast-message');
     }
   
     async goto(url) {
@@ -15,6 +16,10 @@ class LoginPage {
       await this.emailInput.fill(email);
       await this.passwordInput.fill(password);
       await this.loginButton.click();
+    }
+    
+    async assertErrorMessage(expectedMessage) {
+      await expect(this.errorMessage).toContainText(expectedMessage);
     }
   }
   
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,9 +1,18 @@
 import { test, expect } from "@playwright/test";
 import LoginPage from "../pages/LoginPage.js";
+import * as userData from "../fixtures/user.json";
 const URL = process.env.URL;
 const email = process.env.EMAIL;
 const password = process.env.PASSWORD;
 test.describe("login test", () => {
+  userData.invalidUsers.forEach((user, index) => {
+    test(`should show error message for invalid login attempt #${index + 1}`, async ({ page }) => {
+      const loginPage = new LoginPage(page);
+      await loginPage.goto(URL);
+      await loginPage.login(user.email, user.password);
+      await loginPage.assertErrorMessage(user.errorMessage);
+    })
+  });
   test("should login and show successful message", async ({ page }) => {
     const loginPage = new LoginPage(page);
     await loginPage.goto(URL);
